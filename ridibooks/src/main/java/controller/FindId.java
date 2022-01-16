ee2d95278d2073c52e62171b14579ea6bb93ae96
jowonbin <67895294+stshot33@users.noreply.github.com>
2022-01-16 22:11:05 +0900

    결괏값 출력 처리 추가
    - 이메일에 맞는 아이디를 결과값으로 출력해줄 때 앞 2글자 출력 나머지 글자는 *로 처리

--- ridibooks/src/main/java/controller/FindId.java
+++ ridibooks/src/main/java/controller/FindId.java
@@ -6,26 +6,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import controller.Check.RegularExpression;
 import dao.MemberDAO;
-import dto.Member;
 
 @WebServlet("/findId")
 public class FindId extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		this.doPost(request, response);
-	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 
 		MemberDAO memberDAO = MemberDAO.getInstance();
-		Member memberDTO = new Member();
-
 		RegularExpression check = new RegularExpression();
+		HttpSession session = request.getSession();
 
 		String email = request.getParameter("email");
 		int value = memberDAO.selectByIdValue(email);
@@ -34,14 +29,31 @@ public class FindId extends HttpServlet {
 		if (check.mailcheck(email)) {
 			if ( value != 0) {
 				response.setStatus(201);
-				id = memberDAO.selectById(value);
+				id = idchange(memberDAO.selectById(value));
+				System.out.println(id);
+				session.setAttribute("id", id);
+				response.sendRedirect("/jsp/findId2.jsp");
 			} else {
 				response.setStatus(404);
+				System.out.println("해당 이메일 없음");
+				response.sendRedirect("/jsp/findId.jsp");
 			}
 		}else {
 			response.setStatus(400);
+			System.out.println("이메일 형식 재확인");
+			response.sendRedirect("/jsp/findId.jsp");
 		}
+	}
+	
+	public String idchange(String id) {
+		String str1 = id.substring(0, 2);
+		String str2 = id.substring(2, id.length());
+		String result = null;
 		
-		memberDTO.setId(id);
+		str2 = str2.replaceAll("[a-z[0-9]]", "*");
+		
+		result = str1 + str2;
+		
+		return result;
 	}
 }
