8892fc42d6b92dd65fa8a69afcc86b1f73b54c7d
Hong <homepppp@naver.com>
2022-01-26 00:24:49 +0900

    저자소개 페이지 완성

---
+++ ridibooks/WebContent/.history/js/authorInfo_20220126002158.js
@@ -0,0 +1,42 @@
+$(".footer-info a").on("click", function () {
+  let show = $(".footer-info p.invisible");
+  if (show.css("display") == "none") {
+    show.css("display", "block");
+  } else {
+    show.css("display", "none");
+  }
+});
+
+$(".footer-wrapper2 a.a-invisible").on("click", function () {
+  $(".footer-wrapper2 a.a-invisible").css("display", "none");
+  $(".footer-wrapper2 a p.invisible").css("display", "block");
+});
+
+$(".full-input input").on("click", function () {
+  $(".search-list").css("display", "block");
+});
+
+$(".search-input i").on("click", function () {
+  $(".search-list").css("display", "none");
+});
+$(".bg-black").on("click", function () {
+  $(".search-list").css("display", "none");
+});
+$(".response input").on("click", function () {
+  $(".recent-search").css("display", "block");
+});
+
+$("html").on("click", function (e) {
+  let $tgPoint = $(e.target);
+  let $input = $tgPoint.attr("id");
+  let $search = $tgPoint.hasClass(".recent-search");
+  if (!$input && !$search) {
+    $(".recent-search").css("display", "none");
+  }
+});
+
+$(".list-nav ul li").on("click", function (e) {
+  e.preventDefault();
+  $("a.active").removeClass("active");
+  $(e.target).addClass("active");
+});
