0987c81d44561a2ed909990d5bfaa6841a02a52f
jowonbin <67895294+stshot33@users.noreply.github.com>
2022-02-17 09:35:42 +0900

    코드 수정
    코드 수정

--- ridibooks/src/main/java/dao/BookDAO.java
+++ ridibooks/src/main/java/dao/BookDAO.java
@@ -11,9 +11,10 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import dto.BookDTO;
+import dto.CartWishDTO;
 
 public class BookDAO {
-	
+
 	public Connection getConnection() {
 		Connection conn = null;
 
@@ -23,35 +24,44 @@ public class BookDAO {
 			DataSource ds = (DataSource) ic.lookup("java:comp/env/jdbc/ridi");
 
 			conn = ds.getConnection();
-			
+
 		} catch (NamingException | SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 		return conn;
 	}
-	
-	public ArrayList<BookDTO> bookSearch(String q) {
+
+	public ArrayList<BookDTO> bookSearch(String q, int n) {
 		// 도서 검색 결과 조회
 		ArrayList<BookDTO> list = new ArrayList<BookDTO>();
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 
 		try {
-			conn = getConnection();
-
-			String sql = "SELECT * FROM book WHERE b_Name LIKE ?";
-
-			pstmt = conn.prepareStatement(sql);
-
-			pstmt.setString(1, "%"+q+"%");
 			
+			conn = getConnection();
+			if(n==0) {
+				String sql = "SELECT * FROM book WHERE b_Name LIKE ?";
+
+				pstmt = conn.prepareStatement(sql);
+
+				pstmt.setString(1, "%" + q + "%");
+			}else {
+				String sql = "SELECT * FROM book WHERE b_value=?";
+				
+				pstmt = conn.prepareStatement(sql);
+
+				pstmt.setInt(1, n);
+
+			}
 			ResultSet rs = pstmt.executeQuery();
 			
-			while(rs.next()) {
+			
+			while (rs.next()) {
 				BookDTO bookdto = new BookDTO();
-				
+
+				bookdto.setBookvalue(rs.getInt("b_value"));
 				bookdto.setBookimg(rs.getString("b_img"));
 				bookdto.setBname(rs.getString("b_Name"));
 				bookdto.setAuthor(rs.getInt("a_value"));
@@ -62,10 +72,10 @@ public class BookDAO {
 				bookdto.setTotal(rs.getInt("b_Total"));
 				bookdto.setBinfomation(rs.getString("b_Infomation"));
 				bookdto.setBprice(rs.getInt("b_Price"));
-				
+
 				list.add(bookdto);
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
@@ -73,12 +83,136 @@ public class BookDAO {
 				if (pstmt != null)
 					pstmt.close();
 				if (conn != null)
-					pstmt.close();
+					conn.close();
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return list;
+	}
+
+	public boolean selectByList(int value, int bookvalue, int id) {
+		// 카트에 해당 아이디와 책이 있는지 조회
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		boolean result = true;
+		int su = value;
+		String ifSQL = null;
+
+		if (su == 0) {
+			ifSQL = "SELECT * FROM wish WHERE b_value=? AND member_value=?";
+		} else {
+			ifSQL = "SELECT * FROM cart WHERE b_value=? AND member_value=?";
+		}
+
+		try {
+			conn = getConnection();
+
+			String sql = ifSQL;
+
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setInt(1, bookvalue);
+			pstmt.setInt(2, id);
+
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				result = false;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return result;
+	}
+
+	public int bookadd(int value, CartWishDTO cart) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int su = 0;
+		String ifSQL = null;
+
+		if (su == 0) {
+			ifSQL = "INSERT INTO wish(b_value, member_value) VALUES(?, ?)";
+		} else {
+			ifSQL = "INSERT INTO cart(b_value, member_value) VALUES(?, ?)";
+		}
+
+		try {
+			conn = getConnection();
+
+			String sql = ifSQL;
+
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setInt(1, cart.getCbook());
+			pstmt.setInt(2, cart.getCid());
+
+			su = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return su;
+	}
+
+	public int bookdelete(int value, CartWishDTO cart) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int su = value;
+		String ifSQL = null;
+
+		if (su == 0) {
+			ifSQL = "DELETE FROM wish WHERE b_value = ? AND member_value = ?";
+		} else {
+			ifSQL = "DELETE FROM cart WHERE b_value = ? AND member_value = ?";
+		}
+
+		try {
+			conn = getConnection();
+
+			String sql = ifSQL;
+
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setInt(1, cart.getCbook());
+			pstmt.setInt(2, cart.getCid());
+
+			su = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return su;
 	}
 }
