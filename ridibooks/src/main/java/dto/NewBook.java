03e1b9d727d1eacc64babbc9fff4f89f68b0a124
jowonbin <67895294+stshot33@users.noreply.github.com>
2022-03-02 12:01:52 +0900

    코드 추가
    도서 고유 값 / 도서 구매 수 정보 저장 변수 추가

--- ridibooks/src/main/java/dto/NewBook.java
+++ ridibooks/src/main/java/dto/NewBook.java
@@ -1,9 +1,11 @@
 package dto;
 
 public class NewBook {
-	private String img;
-	private String name;
-	private String publisher;
+	private String img;			// 이미지
+	private String name;		// 제목
+	private String publisher;	// 저자
+	private int value;			// 도서 고유값
+	private int Purchase;		// 도서 구매 수
 
 	public String getImg() {
 		return img;
@@ -28,4 +30,21 @@ public class NewBook {
 	public void setPublisher(String publisher) {
 		this.publisher = publisher;
 	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public int getPurchase() {
+		return Purchase;
+	}
+
+	public void setPurchase(int purchase) {
+		Purchase = purchase;
+	}
+
 }
